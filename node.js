0998fb6e49e845bdee72a9cc0468fd1ea88faa57
Advay Anand <advayanand37@gmail.com>
2024-11-12 07:01:18 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,15 @@
+Certain codes for running this program is in this file
+
+1. npm init -y
+
+2. npm install express socket.io
+
+3.  node server.js
+
+4. PS D:\summer training> node server.js
+Server running on http://localhost:3000
+A user connected: rJiMDECiDqCbZUlEAAAB
+A user connected: HvKQra7g7Y517_JGAAAD
+User disconnected: rJiMDECiDqCbZUlEAAAB
+User disconnected: HvKQra7g7Y517_JGAAAD   // This is how this look likes when running on backend
+
